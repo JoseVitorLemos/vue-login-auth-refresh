0a6b2b18d79efe35f561bacb8c64a6ba8fe4aa11
José Vitor <jose81073175@gmail.com>
2022-02-17 23:11:23 -0300

    feat: create simple header

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+      <div>
+				<header />
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+
+header {
+  background-color: var(--color-background-nav);
+  height: 60px;
+  width: 100%;
+}
+
+</style>
+
